8d6166e4571a6a5515598b7e4275d84bd5d32adb
Papali Priya Ratan Sahu <sahu.ppr@gmail.com>
2024-09-28 23:20:22 +0530

    Updated: Routes and packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,23 @@
 import Booking from "./Booking"
 import Slots from "./components/Slots"
 import Button from 'react-bootstrap/Button'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'leaflet/dist/leaflet.css';
 import './App.css';
 import Map from "./Components/MapPage"
+import Registration from './Registration';
 function App() {
 
   return (
     <>
       {/* <Booking/> */}
+      <Router>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/booking" element={<Booking />} />
+      </Routes>
+    </Router>
       <Slots/>
       <Button>Text Button</Button>
       <Map/>
